6ef243d82a3a537ff8d3b355d52da97506ebaf98
alvildamkopp-gif <alvildamkopp@gmail.com>
2026-08-19 10:39:29 +0200

    Update kereby_bot.py

--- kereby_bot.py
+++ kereby_bot.py
@@ -1,1 +1,17 @@
+import os
+import requests
 
+TOKEN = os.environ["TELEGRAM_BOT_TOKEN"]
+CHAT_ID = os.environ["TELEGRAM_CHAT_ID"]
+
+message = "🏠 TEST! Kereby-botten virker! 🤖🔔"
+
+url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+
+requests.post(
+    url,
+    data={
+        "chat_id": CHAT_ID,
+        "text": message,
+    },
+)
