de5fa0dc7781e41b8c10a70f9e9e099a94784525
alvildamkopp-gif <alvildamkopp@gmail.com>
2026-08-19 12:39:09 +0200

    Update kereby_bot.py

--- kereby_bot.py
+++ kereby_bot.py
@@ -1,19 +1,18 @@
 import requests
-from bs4 import BeautifulSoup
+import re
 
 URL = "https://kereby.dk/bolig/"
 
 response = requests.get(URL, timeout=20)
 response.raise_for_status()
 
-soup = BeautifulSoup(response.text, "html.parser")
-
 print("Kereby blev hentet!")
-print("Antal links fundet:", len(soup.find_all("a")))
+print("Status:", response.status_code)
 
-for link in soup.find_all("a", href=True):
-    href = link["href"]
-    text = link.get_text(" ", strip=True)
+links = re.findall(r'href=["\']([^"\']+)["\']', response.text)
 
-    if "/bolig/" in href and text:
-        print(text[:100], "→", href)
+print("Antal links fundet:", len(links))
+
+for link in links:
+    if "/bolig/" in link:
+        print(link)
